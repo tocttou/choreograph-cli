03291df769c29dbb9f895d52384713b718b603db
Ashish <me@ashishchaudhary.in>
2016-08-13 09:24:16 +0530

    added command: verify to check .choreo.yml

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-const name = 'Ashish';
-console.log(`hello ${name}`);
+import vorpal from 'vorpal';
+
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const vcl = vorpal();
+const chalk = vorpal().chalk;
+const Log = console.log;
+
+vcl
+  .delimiter(chalk.cyan('choreo$'))
+  .show()
+  .parse(process.argv);
+
+// Commands
+
+vcl
+  .command('verify', 'Verifies the config file.')
+  .action((args, callback) => {
+    try {
+      const fileStats = fs.statSync(`${process.cwd()}/.choreo.yml`);
+      try {
+        const doc = yaml.load(fs.readFileSync(`${process.cwd()}/.choreo.yml`, 'utf-8'));
+      } catch (err) {
+        Log('Unable to parse .choreo.yml');
+      }
+    } catch (err) {
+      Log('file not found: .choreo.yml');
+    }
+
+    Log(chalk.green('.choreo.yml is valid'));
+
+  });
